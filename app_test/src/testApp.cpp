e06135aaec096179ff8ef0add92793d0fcb0f385
Josh Ashby <joshuaashby@joshashby.com>
2009-12-19 16:52:14 -0700

    started to make a simple serial send app for my robot, may end up being a full out gui later on

--- app_test/src/testApp.cpp
+++ app_test/src/testApp.cpp
@@ -1,8 +1,17 @@
 #include "testApp.h"
-
+char forwardcolor;
+char backcolor;
+char leftcolor;
+char rightcolor;
 //--------------------------------------------------------------
 void testApp::setup(){
-
+    ofBackground(255,255,255);
+	ofSetWindowTitle("Navigation");
+	ofSetFrameRate(60);
+	forwardcolor = false;
+	backcolor = false;
+	leftcolor = false;
+	rightcolor = false;
 }
 
 //--------------------------------------------------------------
@@ -12,17 +21,80 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    if (forwardcolor){
+        ofSetColor(0x456456);
+    } else {
+        ofSetColor(0x092345);
+    }
+	ofFill();
+	ofCircle(200,75,50);
+	ofSetColor(0x000000);
+	ofDrawBitmapString("Forward",175,75);
 
+
+	if (leftcolor){
+        ofSetColor(0x456456);
+    } else {
+        ofSetColor(0x092345);
+    }
+	ofFill();
+	ofCircle(100,125,50);
+	ofSetColor(0x000000);
+	ofDrawBitmapString("Left",75,125);
+
+
+	if (backcolor){
+        ofSetColor(0x456456);
+    } else {
+        ofSetColor(0x092345);
+    }
+	ofFill();
+	ofCircle(200,175,50);
+	ofSetColor(0x000000);
+	ofDrawBitmapString("Back",175,175);
+
+
+	if (rightcolor){
+        ofSetColor(0x456456);
+    } else {
+        ofSetColor(0x092345);
+    }
+	ofFill();
+	ofCircle(300,125,50);
+	ofSetColor(0x000000);
+	ofDrawBitmapString("Right",275,125);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-
+	if (key == 'w'){
+        forwardcolor = !forwardcolor;
+	}
+	if (key == 's'){
+        backcolor = !backcolor;
+	}
+	if (key == 'a'){
+        leftcolor = !leftcolor;
+	}
+	if (key == 'd'){
+        rightcolor = !rightcolor;
+	}
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+	if (key == 'w'){
+        forwardcolor = !forwardcolor;
+	}
+	if (key == 's'){
+        backcolor = !backcolor;
+	}
+	if (key == 'a'){
+        leftcolor = !leftcolor;
+	}
+	if (key == 'd'){
+        rightcolor = !rightcolor;
+	}
 }
 
 //--------------------------------------------------------------
